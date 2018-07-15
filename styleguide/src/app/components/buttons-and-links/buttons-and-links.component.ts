06e55a4b41a0644af2ed3796d0d51f4c7f20964c
Dinesh Matta <dineshdm7@gmail.com>
2018-07-15 15:34:04 +0800

    Adding readme and fixing some issues

--- styleguide/src/app/components/buttons-and-links/buttons-and-links.component.ts
+++ styleguide/src/app/components/buttons-and-links/buttons-and-links.component.ts
@@ -10,8 +10,8 @@ export class ButtonsAndLinksComponent implements OnInit {
   constructor(private escapeHtml: EscapeHtmlService) {}
 
   codePreview1 =
-    '<button class="ac-button primary">< i class = "button__icon ac-icon-add" > </i>Add</button>\n' +
-    '<button class="ac-button primary" disabled>< i class = "button__icon ac-icon-add" > </i>Add</button>\n' +
+    '<button class="ac-button primary"><i class="button__icon ac-icon-add" > </i>Add</button>\n' +
+    '<button class="ac-button primary" disabled><i class = "button__icon ac-icon-add" > </i>Add</button>\n' +
     '<button class="ac-button primary">Next</button>\n' +
     '<button class="ac-button primary" disabled>Next</button>\n' +
     '<button class="ac-button">Save</button>\n' +
@@ -19,18 +19,18 @@ export class ButtonsAndLinksComponent implements OnInit {
     '<button class="ac-button primary lg">Login in</button>';
 
   codePreview2 =
-    '<button class="ac-button secondary">< i class = "button__icon ac-icon-add" > </i>Add</button>\n' +
-    '<button class="ac-button secondary" disabled>< i class = "button__icon ac-icon-add" > </i>Add</button>\n' +
-    '<button class="ac-button secondary danger">< i class = "button__icon ac-icon-delete" > </i>Delete Confiuration</button>\n' +
-    '<button class="ac-button secondary danger" disabled>< i class = "button__icon ac-icon-delete" > </i>Delete Confiuration</button>\n' +
-    '<button class="ac-button normal-fill sm">< i class = "button__icon ac-icon-play" > </i>Run</button>\n' +
+    '<button class="ac-button secondary"><i class="button__icon ac-icon-add" > </i>Add</button>\n' +
+    '<button class="ac-button secondary" disabled><i class="button__icon ac-icon-add" > </i>Add</button>\n' +
+    '<button class="ac-button secondary danger"><i class="button__icon ac-icon-delete" > </i>Delete Confiuration</button>\n' +
+    '<button class="ac-button secondary danger" disabled><i class="button__icon ac-icon-delete" > </i>Delete Confiuration</button>\n' +
+    '<button class="ac-button normal-fill sm"><i class="button__icon ac-icon-play" > </i>Run</button>\n' +
     '<button class="ac-button normal-fill">Add a Confiuration</button>\n' +
     '<button class="ac-button normal">Register</button>\n' +
-    '<button class="ac-float-button" aria-label="Favorite">< i class = "button__icon ac-icon-delete" > </i></button>';
+    '<button class="ac-float-button" aria-label="Favorite"><i class="button__icon ac-icon-delete" > </i></button>';
 
   codePreview3 =
-    '<div class="element pull-left">< a href = "javascript:;" class = "ac-link" > Cancel < /a></div>\n' +
-    '<a href="javascript:;" class="ac-link sm active">< i class = "link__icon ac-icon-refresh" > </i>Refresh All</a>\n' +
+    '<div class="element pull-left"><a href = "javascript:;" class="ac-link" > Cancel < /a></div>\n' +
+    '<a href="javascript:;" class="ac-link sm active">< i class="link__icon ac-icon-refresh" > </i>Refresh All</a>\n' +
     '<a href="javascript:;" class="ac-link">Cancel</a>\n' +
     '<button class="ac-button danger-fill">Delete Forever</button>\n' +
     '<a href="javascript:;" class="ac-link">Cancel</a>\n' +
